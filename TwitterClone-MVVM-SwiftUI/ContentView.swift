350865ccc3ff624bda25a2f1ff260a95c248afd2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-10 14:37:01 -0800

    Initial Commit

---
+++ TwitterClone-MVVM-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterClone-MVVM-SwiftUI/TwitterClone_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterClone_MVVM_SwiftUIApp.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TwitterClone_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
