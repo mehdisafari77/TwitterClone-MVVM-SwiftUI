5043b49bd642cdf98e5558e435a78b523bec6bd1
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-10 14:49:51 -0800

    add Firebase config

--- TwitterClone-MVVM-SwiftUI/TwitterClone_MVVM_SwiftUIApp.swift
+++ TwitterClone-MVVM-SwiftUI/TwitterClone_MVVM_SwiftUIApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterClone_MVVM_SwiftUIApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
