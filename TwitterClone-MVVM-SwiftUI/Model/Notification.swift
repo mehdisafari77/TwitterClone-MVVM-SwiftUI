a5b7afcb3afef465e0a4446fb0d186d1ab4d156c
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-19 10:25:50 -0700

    create notification model

---
+++ TwitterClone-MVVM-SwiftUI/Model/Notification.swift
@@ -0,0 +1,45 @@
+//
+//  Notification.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 19/03/2022.
+//
+
+import SwiftUI
+
+struct Notification: Identifiable {
+    let id: String
+    let uid: String
+    let username: String
+    let profileImageUrl: String
+    let type: NotificationType
+    var tweetId: String?
+    var tweet: Tweet?
+    var userIsFollowed = false
+    
+    init(dictionary: [String: Any]) {
+        self.id = dictionary["id"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.type = NotificationType(rawValue: dictionary["type"] as? Int ?? 0) ?? .like
+        
+        if let tweetId = dictionary["tweetId"] as? String {
+            self.tweetId = tweetId
+        }
+    }
+}
+
+enum NotificationType: Int {
+    case like
+    case reply
+    case follow
+    
+    var notificationText: String {
+        switch self {
+        case .like: return " liked one of your tweets"
+        case .reply: return " replied to one of your tweets"
+        case .follow: return " started following you"
+        }
+    }
+}
