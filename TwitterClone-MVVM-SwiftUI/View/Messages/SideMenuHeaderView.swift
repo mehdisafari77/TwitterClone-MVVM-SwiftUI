09755aaa3005e15cd8aa86b71d48e1bfdd02a8e0
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-30 18:07:27 -0700

    side menu finalized

--- TwitterClone-MVVM-SwiftUI/View/Messages/SideMenuHeaderView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/SideMenuHeaderView.swift
@@ -6,15 +6,66 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct SideMenuHeaderView: View {
+    private let user = AuthViewModel.shared.user
+    @Binding var show: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack(alignment: .topTrailing) {
+            Button(action: {
+                withAnimation(.spring()){
+                    self.show.toggle()
+                }
+            }) {
+                Image(systemName: "xmark")
+                    .font(.system(size: 24))
+                    .scaledToFill()
+                    .foregroundColor(.black)
+                    .shadow(color: .gray, radius: 10, x: 0, y: 0)
+            }.padding()
+            
+            VStack(alignment: .leading) {
+                KFImage(URL(string: user?.profileImageUrl ?? ""))
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(28)
+                
+                Text(user?.fullname ?? "")
+                    .font(.system(size: 16, weight: .semibold))
+                
+                Text("@\(user?.username ?? "")")
+                    .foregroundColor(.gray)
+                    .font(.system(size: 15))
+                    .padding(.bottom)
+                
+                HStack(spacing: 12) {
+                    HStack(spacing: 4) {
+                        Text("\(user?.stats.following ?? 0)").bold()
+                        Text("Following")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack(spacing: 4) {
+                        Text("\(user?.stats.followers ?? 0)").bold()
+                        Text("Followers")
+                            .foregroundColor(.gray)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }.padding()
+        }
     }
 }
 
 struct SideMenuHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        SideMenuHeaderView()
+        SideMenuHeaderView(show: .constant(false))
     }
 }
+
