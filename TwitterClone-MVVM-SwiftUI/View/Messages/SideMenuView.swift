dc2c7a720500d245d991c74f75bd4cb70e4fafc1
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-30 18:08:35 -0700

    sideMenu View file added

---
+++ TwitterClone-MVVM-SwiftUI/View/Messages/SideMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 30/03/2022.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
