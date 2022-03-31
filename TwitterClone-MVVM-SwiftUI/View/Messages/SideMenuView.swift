2a348c71c04f3843558d6281a72ed82b2c91c1d2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-30 18:08:49 -0700

    sideMenuVIew finalized

--- TwitterClone-MVVM-SwiftUI/View/Messages/SideMenuView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/SideMenuView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct SideMenuView: View {
+    @Binding var isShowing: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let user = AuthViewModel.shared.user {
+            VStack {
+                SideMenuHeaderView(show: $isShowing)
+                    .frame(height: 200)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 12) {
+                        ForEach(SideMenuViewModel.allCases, id: \.self) { option in
+                            if option == .profile {
+                                NavigationLink(
+                                    destination: LazyView(UserProfileView(user: user)),
+                                    label: {
+                                        SideMenuOptionView(option: option)
+                                    })
+                                .frame(height: 40)
+                                .padding()
+                            } else if option == .logout {
+                                Button(action: { AuthViewModel.shared.signOut() }) {
+                                    SideMenuOptionView(option: option)
+                                        .frame(height: 40)
+                                        .padding()
+                                }
+                            }
+                        }
+                    }
+                }
+                .navigationBarHidden(true)
+            }
+        }
     }
 }
 
-struct SideMenuView_Previews: PreviewProvider {
-    static var previews: some View {
-        SideMenuView()
-    }
-}
