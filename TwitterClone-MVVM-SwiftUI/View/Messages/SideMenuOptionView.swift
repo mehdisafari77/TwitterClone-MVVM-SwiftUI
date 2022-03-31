0fa9b6e6a207cd00fc973f346f7d004ecd4e8e44
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-30 18:07:51 -0700

    side option view created

---
+++ TwitterClone-MVVM-SwiftUI/View/Messages/SideMenuOptionView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuOptionView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 30/03/2022.
+//
+
+import SwiftUI
+
+struct SideMenuOptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuOptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuOptionView()
+    }
+}
