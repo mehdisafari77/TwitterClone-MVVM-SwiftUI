598a4953e04681116180885db6b4050d8aa5933b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-31 17:00:33 -0700

    register user auth done

--- TwitterClone-MVVM-SwiftUI/ViewModels/AuthViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/AuthViewModel.swift
@@ -32,3 +32,42 @@ class AuthViewModel: ObservableObject {
             self.fetchUser()
         }
     }
+    
+    func registerUser(email: String, password: String, username: String,
+                      fullname: String, profileImage: UIImage) {
+        
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child(filename)
+        
+        storageRef.putData(imageData, metadata: nil) { _, error in
+            if let error = error {
+                print("DEBUG: Failed to upload image \(error.localizedDescription)")
+                return
+            }
+                        
+            storageRef.downloadURL { url, _ in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                    if let error = error {
+                        print("DEBUG: Error \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let user = result?.user else { return }
+                    
+                    let data = ["email": email,
+                                "username": username.lowercased(),
+                                "fullname": fullname,
+                                "profileImageUrl": profileImageUrl,
+                                "uid": user.uid]
+                    
+                    Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                        self.userSession = user
+                        self.fetchUser()
+                    }
+                }
+            }
+        }
+    }
