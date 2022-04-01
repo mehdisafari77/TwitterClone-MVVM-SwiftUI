7e54001a33b97bbb1362b2f9154d173c754ee3e6
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-31 17:03:07 -0700

    fetch user in searchVM done

--- TwitterClone-MVVM-SwiftUI/ViewModels/SearchViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/SearchViewModel.swift
@@ -5,4 +5,33 @@
 //  Created by Mehdi MMS on 31/03/2022.
 //
 
-import Foundation
+import SwiftUI
+import Firebase
+
+enum SearchViewModelConfiguration {
+    case search
+    case newMessage
+}
+
+class SearchViewModel: ObservableObject {
+    @Published var users = [User]()
+    private let config: SearchViewModelConfiguration
+    
+    init(config: SearchViewModelConfiguration) {
+        self.config = config
+        fetchUsers(forConfig: config)
+    }
+    
+    func fetchUsers(forConfig config: SearchViewModelConfiguration) {
+        COLLECTION_USERS.getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+            let users = documents.map({ User(dictionary: $0.data()) })
+            
+            switch config {
+            case .newMessage:
+                self.users = users.filter({ !$0.isCurrentUser })
+            case .search:
+                self.users = users
+            }
+        }
+    }
