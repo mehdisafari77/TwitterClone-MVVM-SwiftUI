c34fb3dd3d933a04559fcdbca99e5cb7b53f1dfa
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-31 17:05:45 -0700

    uploading notification func added in notification VM

--- TwitterClone-MVVM-SwiftUI/ViewModels/NotificationViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/NotificationViewModel.swift
@@ -5,4 +5,32 @@
 //  Created by Mehdi MMS on 31/03/2022.
 //
 
-import Foundation
+import Firebase
+
+class NotificationViewModel: ObservableObject {
+    @Published var notifications = [Notification]()
+    
+    init() {
+        fetchNotifications()
+    }
+    
+    static func uploadNotification(toUid uid: String, type: NotificationType, tweet: Tweet? = nil) {
+        guard let currentUser = AuthViewModel.shared.user else { return }
+        guard uid != currentUser.id else { return }
+        
+        let docRef = COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").document()
+        
+        var data: [String: Any] = ["timestamp": Timestamp(date: Date()),
+                                   "uid": currentUser.id,
+                                   "type": type.rawValue,
+                                   "id": docRef.documentID,
+                                   "profileImageUrl": currentUser.profileImageUrl,
+                                   "username": currentUser.username]
+        
+        if let tweet = tweet {
+            data["tweetId"] = tweet.id
+        }
+        
+        docRef.setData(data)
+    }
+    
