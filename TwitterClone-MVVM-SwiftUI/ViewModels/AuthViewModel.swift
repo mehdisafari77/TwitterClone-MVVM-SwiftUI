3c5fa07daf7d98cdca1db1ca8be21af686977c34
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-31 17:00:14 -0700

    login auth done

--- TwitterClone-MVVM-SwiftUI/ViewModels/AuthViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/AuthViewModel.swift
@@ -5,4 +5,30 @@
 //  Created by Mehdi MMS on 31/03/2022.
 //
 
-import Foundation
+import SwiftUI
+import Firebase
+
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    @Published var isAuthenticating = false
+    @Published var error: Error?
+    @Published var user: User?
+    
+    static let shared = AuthViewModel()
+    
+    init() {
+        userSession = Auth.auth().currentUser
+        fetchUser()
+    }
+    
+    func login(withEmail email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("DEBUG: Failed to login: \(error.localizedDescription)")
+                return
+            }
+            
+            self.userSession = result?.user
+            self.fetchUser()
+        }
+    }
