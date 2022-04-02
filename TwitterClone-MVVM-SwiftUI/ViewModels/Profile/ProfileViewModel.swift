9627acd1de58e400001ac79d86f1e2418c5fde44
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-01 19:37:46 -0700

    tweets function added

--- TwitterClone-MVVM-SwiftUI/ViewModels/Profile/ProfileViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/Profile/ProfileViewModel.swift
@@ -5,4 +5,29 @@
 //  Created by Mehdi MMS on 01/04/2022.
 //
 
-import Foundation
+import SwiftUI
+import Firebase
+
+class ProfileViewModel: ObservableObject {
+    @Published var user: User
+    @Published var userTweets = [Tweet]()
+    @Published var likedTweets = [Tweet]()
+    @Published var replies = [Tweet]()
+    
+    init(user: User) {
+        self.user = user
+        checkIfUserIsFollowed()
+        fetchUserTweets()
+        fetchLikedTweets()
+        fetchUserStats()
+        fetchReplies()
+    }
+        
+    func tweets(forFilter filter: TweetFilterOptions) -> [Tweet] {
+        switch filter {
+        case .tweets: return userTweets
+        case .likes: return likedTweets
+        case .replies: return replies
+        }
+    }
+}
