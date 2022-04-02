d4a8e40f5e68583c27d79def567d4381e761a375
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-01 19:40:24 -0700

    finish profile edit vm

--- TwitterClone-MVVM-SwiftUI/ViewModels/Profile/EditProfileViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/Profile/EditProfileViewModel.swift
@@ -5,4 +5,37 @@
 //  Created by Mehdi MMS on 01/04/2022.
 //
 
-import Foundation
+import SwiftUI
+
+class EditProfileViewModel: ObservableObject {
+    let user: User
+    
+    init(user: User) {
+        self.user = user
+    }
+    
+}
+
+enum EditProfileOptions: Int, CaseIterable {
+    case fullname
+    case username
+    case bio
+    
+    var description: String {
+        switch self {
+        case .fullname: return "Full Name"
+        case .username: return "Username"
+        case .bio: return "Bio"
+        }
+    }
+    
+    func optionValue(user: User) -> String {
+        switch self {
+        case .fullname: return user.fullname
+        case .username: return user.username
+        case .bio: return user.bio ?? "Enter your bio"
+        }
+    }
+}
+
+
