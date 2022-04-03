085afe860a35e1c5d933de4d6f3b8ea0f75133fc
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-02 23:08:36 -0700

    send messages func implemented in chatVM

--- TwitterClone-MVVM-SwiftUI/ViewModels/Messages/ChatViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/Messages/ChatViewModel.swift
@@ -38,3 +38,26 @@ class ChatViewModel: ObservableObject {
             }
         }
     }
+    
+    func sendMessage(_ messageText: String) {
+        guard let currentUid = AuthViewModel.shared.userSession?.uid else { return }
+        let currentUserRef = COLLECTION_MESSAGES.document(currentUid).collection(user.id).document()
+        let receivingUserRef = COLLECTION_MESSAGES.document(user.id).collection(currentUid)
+        let receivingRecentRef = COLLECTION_MESSAGES.document(user.id).collection("recent-messages")
+        let currentRecentRef =  COLLECTION_MESSAGES.document(currentUid).collection("recent-messages")
+        
+        let messageID = currentUserRef.documentID
+        
+        let data: [String: Any] = ["text": messageText,
+                                   "id": messageID,
+                                   "fromId": currentUid,
+                                   "toId": user.id,
+                                   "timestamp": Timestamp(date: Date())]
+        
+        currentUserRef.setData(data)
+        receivingUserRef.document(messageID).setData(data)
+        receivingRecentRef.document(currentUid).setData(data)
+        currentRecentRef.document(user.id).setData(data)
+    }
+}
+
