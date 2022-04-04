b1fe25ce961406981195274fbd7eb4cf4f4c5898
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-03 21:35:44 -0700

    text area view added

---
+++ TwitterClone-MVVM-SwiftUI/Utils/Views/TextArea.swift
@@ -0,0 +1,20 @@
+//
+//  TextArea.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 03/04/2022.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextArea_Previews: PreviewProvider {
+    static var previews: some View {
+        TextArea()
+    }
+}
