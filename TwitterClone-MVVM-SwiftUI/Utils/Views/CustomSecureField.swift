ff4db7a32f47b731475fd8fe6a0e0c466f4ab2b2
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-04 20:43:33 -0700

    create custom secure field view

---
+++ TwitterClone-MVVM-SwiftUI/Utils/Views/CustomSecureField.swift
@@ -0,0 +1,20 @@
+//
+//  CustomSecureField.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 04/04/2022.
+//
+
+import SwiftUI
+
+struct CustomSecureField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomSecureField_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSecureField()
+    }
+}
