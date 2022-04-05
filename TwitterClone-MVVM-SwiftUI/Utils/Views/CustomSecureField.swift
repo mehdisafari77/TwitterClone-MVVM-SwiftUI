a546c50368a3d0a1b08aa83fe9767bb323e874c7
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-04 20:43:53 -0700

    finalized custom secure field

--- TwitterClone-MVVM-SwiftUI/Utils/Views/CustomSecureField.swift
+++ TwitterClone-MVVM-SwiftUI/Utils/Views/CustomSecureField.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct CustomSecureField: View {
+    @Binding var text: String
+    let placeholder: Text
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            if text.isEmpty {
+                placeholder
+                    .foregroundColor(Color(.init(white: 1, alpha: 0.87)))
+                    .padding(.leading, 40)
+            }
+            
+            HStack(spacing: 16) {
+                Image(systemName: "lock")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                    .foregroundColor(.white)
+                
+                SecureField("", text: $text)
+            }
+        }
     }
 }
 
-struct CustomSecureField_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomSecureField()
-    }
-}
