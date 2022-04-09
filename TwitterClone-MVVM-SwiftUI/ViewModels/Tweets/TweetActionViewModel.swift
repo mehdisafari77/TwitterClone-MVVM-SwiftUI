bbd17de1799671068a57a348dd41299a4c1ba0da
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-09 06:54:34 -0700

    finalize tweet action vm

--- TwitterClone-MVVM-SwiftUI/ViewModels/Tweets/TweetActionViewModel.swift
+++ TwitterClone-MVVM-SwiftUI/ViewModels/Tweets/TweetActionViewModel.swift
@@ -5,4 +5,57 @@
 //  Created by Mehdi MMS on 09/04/2022.
 //
 
-import Foundation
+import SwiftUI
+import Firebase
+
+class TweetActionViewModel: ObservableObject {
+    let tweet: Tweet
+    @Published var didLike = false
+    
+    init(tweet: Tweet) {
+        self.tweet = tweet
+        checkIfUserLikedTweet()
+    }
+    
+    func likeTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let tweetLikesRef = COLLECTION_TWEETS.document(tweet.id).collection("tweet-likes")
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes")
+        
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes + 1]) { _ in
+            tweetLikesRef.document(uid).setData([:]) { _ in
+                userLikesRef.document(self.tweet.id).setData([:]) { _ in
+                    self.didLike = true
+                    
+                    NotificationViewModel.uploadNotification(toUid: self.tweet.uid, type: .like, tweet: self.tweet)
+                }
+            }
+        }
+    }
+    
+    func unlikeTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let tweetLikesRef = COLLECTION_TWEETS.document(tweet.id).collection("tweet-likes")
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes")
+        
+        COLLECTION_TWEETS.document(tweet.id).updateData(["likes": tweet.likes - 1]) { _ in
+            tweetLikesRef.document(uid).delete { _ in
+                userLikesRef.document(self.tweet.id).delete { _ in
+                    self.didLike = false
+                    
+                    NotificationViewModel.deleteNotification(toUid: self.tweet.uid, type: .like, tweetId: self.tweet.id)
+                }
+            }
+        }
+    }
+    
+    func checkIfUserLikedTweet() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        let userLikesRef = COLLECTION_USERS.document(uid).collection("user-likes").document(tweet.id)
+        
+        userLikesRef.getDocument { snapshot, _ in
+            guard let didLike = snapshot?.exists else { return }
+            self.didLike = didLike
+        }
+    }
+}
