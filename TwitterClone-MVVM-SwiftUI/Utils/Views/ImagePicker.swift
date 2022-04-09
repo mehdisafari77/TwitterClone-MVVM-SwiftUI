810b647588b1346e55cd612926cbae66126445d7
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-09 06:46:37 -0700

    create image picker file

---
+++ TwitterClone-MVVM-SwiftUI/Utils/Views/ImagePicker.swift
@@ -0,0 +1,20 @@
+//
+//  ImagePicker.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 09/04/2022.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImagePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ImagePicker()
+    }
+}
