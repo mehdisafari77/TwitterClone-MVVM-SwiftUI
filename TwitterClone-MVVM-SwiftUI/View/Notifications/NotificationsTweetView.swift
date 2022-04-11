318a3f4b72badcdd224a5d288480de8f1a63ef6b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-10 19:46:24 -0700

    NotificationsView tweet added

---
+++ TwitterClone-MVVM-SwiftUI/View/Notifications/NotificationsTweetView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsTweetView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 10/04/2022.
+//
+
+import SwiftUI
+
+struct NotificationsTweetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationsTweetView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsTweetView()
+    }
+}
