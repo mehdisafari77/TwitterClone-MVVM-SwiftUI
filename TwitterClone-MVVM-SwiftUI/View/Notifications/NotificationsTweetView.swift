966934c03c44e4c2aef2dc454d64a42eb8a0488b
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-10 19:46:47 -0700

    finished tweet view notifications

--- TwitterClone-MVVM-SwiftUI/View/Notifications/NotificationsTweetView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Notifications/NotificationsTweetView.swift
@@ -6,15 +6,38 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
-struct NotificationsTweetView: View {
+struct NotificationTweetView: View {
+    let tweet: Tweet
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            HStack {
+                KFImage(URL(string: tweet.profileImageUrl))
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 24, height: 24)
+                    .clipShape(Circle())
+                
+                Text(tweet.fullname)
+                    .font(.system(size: 12, weight: .semibold))
+                
+                Text("• @\(tweet.username)")
+                    .font(.system(size: 12))
+                    .foregroundColor(.gray)
+            }
+            
+            Text(tweet.caption)
+                .font(.system(size: 14))
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 3)
+                .stroke(Color(.systemGray4), lineWidth: 1)
+        )
+        .padding()
     }
 }
 
-struct NotificationsTweetView_Previews: PreviewProvider {
-    static var previews: some View {
-        NotificationsTweetView()
-    }
-}
