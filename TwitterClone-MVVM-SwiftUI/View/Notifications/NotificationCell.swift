bfd94e2191c8ca02ea15ff4eb78736561af053ad
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-10 19:47:37 -0700

    notification Cell view completed

--- TwitterClone-MVVM-SwiftUI/View/Notifications/NotificationCell.swift
+++ TwitterClone-MVVM-SwiftUI/View/Notifications/NotificationCell.swift
@@ -6,15 +6,48 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct NotificationCell: View {
+    let notification: Notification
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                KFImage(URL(string: notification.profileImageUrl))
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 36, height: 36)
+                    .clipShape(Circle())
+                
+                HStack {
+                    Text(notification.username).font(.system(size: 14, weight: .semibold)) +
+                        Text(notification.type.notificationText).font(.system(size: 15))
+                }.padding(.horizontal, 4)
+                
+                Spacer()
+                                
+                if notification.type == .follow {
+                    Button(action: {}, label: {
+                        Text(notification.userIsFollowed ? "Following" : "Follow")
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 8)
+                            .background(Color(.systemBlue))
+                            .clipShape(Capsule())
+                            .foregroundColor(.white)
+                            .font(.system(size: 14, weight: .semibold))
+                    })
+                }
+                
+            }.padding(.horizontal, 4)
+            
+            if let tweet = notification.tweet {
+                NotificationTweetView(tweet: tweet)
+            }
+            
+            Divider()
+        }
     }
 }
 
-struct NotificationCell_Previews: PreviewProvider {
-    static var previews: some View {
-        NotificationCell()
-    }
-}
