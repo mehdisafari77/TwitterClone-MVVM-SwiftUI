92efe8209c26fd11f65c24a2864284490c8d42c6
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-10 19:47:17 -0700

    notificationCell added

---
+++ TwitterClone-MVVM-SwiftUI/View/Notifications/NotificationCell.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationCell.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 10/04/2022.
+//
+
+import SwiftUI
+
+struct NotificationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationCell()
+    }
+}
