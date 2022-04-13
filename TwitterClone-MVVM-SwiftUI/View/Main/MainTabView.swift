4842911295dca99c474d8aa3d445b6b225cba41a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-04-12 19:17:46 -0700

    add mantas view code

--- TwitterClone-MVVM-SwiftUI/View/Main/MainTabView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Main/MainTabView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct MainTabView: View {
+    @Binding var selectedIndex: Int
+    @ObservedObject var viewModel: FeedViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct MainTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainTabView()
+        TabView(selection: $selectedIndex) {
+            FeedView(viewModel: viewModel)
+                .onTapGesture {
+                    selectedIndex = 0
+                }
+                .tabItem {
+                    Image(systemName: "house")
+                }.tag(0)
+            
+            SearchView()
+                .onTapGesture {
+                    selectedIndex = 1
+                }
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }.tag(1)
+            
+            NotificationsView()
+                .onTapGesture {
+                    selectedIndex = 2
+                }
+                .tabItem {
+                    Image(systemName: "heart")
+                }.tag(2)
+            
+            ConversationsView()
+                .onTapGesture {
+                    selectedIndex = 3
+                }
+                .tabItem {
+                    Image(systemName: "envelope")
+                }.tag(3)
+        }
     }
 }
