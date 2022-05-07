6198a11ea78a30323af01e411329f300a8b224a6
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-07 16:09:15 -0700

    add the tweet cell

---
+++ TwitterClone-MVVM-SwiftUI/View/Tweets/TweetCell.swift
@@ -0,0 +1,52 @@
+//
+//  TweetCell.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 07/05/2022.
+//
+
+import SwiftUI
+import KingfisherSwiftUI
+
+struct TweetCell: View {
+    let tweet: Tweet
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top, spacing: 12) {
+                KFImage(URL(string: tweet.profileImageUrl))
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(56 / 2)
+                    .padding(.leading)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text(tweet.fullname)
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.black)
+                        
+                        Text("@\(tweet.username) •")
+                            .foregroundColor(.gray)
+                        
+                        Text(tweet.timestampString)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Text(tweet.caption)
+                        .foregroundColor(.black)
+                }
+            }
+            .padding(.bottom)
+            .padding(.trailing)
+            
+            TweetActionsView(tweet: tweet)
+            
+            Divider()
+        }
+        .padding(.leading, -16)
+    }
+}
+
