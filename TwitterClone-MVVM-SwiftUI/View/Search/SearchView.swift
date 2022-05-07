59bdd1e1a9829130547b66ee8d47edae7735184a
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-05-07 16:07:45 -0700

    add SearchView

---
+++ TwitterClone-MVVM-SwiftUI/View/Search/SearchView.swift
@@ -0,0 +1,39 @@
+//
+//  SearchView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 07/05/2022.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    @State var searchText = ""
+    @ObservedObject var viewModel = SearchViewModel(config: .search)
+
+    var body: some View {
+        ScrollView {
+            SearchBar(text: $searchText)
+                .padding()
+
+            VStack(alignment: .leading) {
+                ForEach(searchText.isEmpty ? viewModel.users : viewModel.filteredUsers(searchText)) { user in
+                    HStack { Spacer() }
+                    
+                    NavigationLink(
+                        destination: LazyView(UserProfileView(user: user)),
+                        label: {
+                            UserCell(user: user)
+                        })
+                }
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
