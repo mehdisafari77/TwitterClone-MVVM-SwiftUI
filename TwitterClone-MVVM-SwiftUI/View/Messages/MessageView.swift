5d8b5920c698c2af901efbed808cdbce5f95a068
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:23:18 -0700

    message view added

--- TwitterClone-MVVM-SwiftUI/View/Messages/MessageView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/MessageView.swift
@@ -6,15 +6,43 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct MessageView: View {
+    let message: Message
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct MessageView_Previews: PreviewProvider {
-    static var previews: some View {
-        MessageView()
+        HStack {
+            if message.isFromCurrentUser {
+                Spacer()
+                Text(message.text)
+                    .padding()
+                    .background(Color.blue)
+                    .clipShape(ChatBubble(isFromCurrentUser: true))
+                    .foregroundColor(.white)
+                    .padding(.horizontal)
+                    .padding(.leading, 100)
+                    .padding(.trailing, 16)
+            } else {
+                HStack(alignment: .bottom) {
+                    KFImage(URL(string: message.user.profileImageUrl))
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 40)
+                        .clipShape(Circle())
+                    
+                    Text(message.text)
+                        .padding()
+                        .background(Color(.systemGray5))
+                        .clipShape(ChatBubble(isFromCurrentUser: false))
+                        .foregroundColor(.black)
+                    
+                }
+                .padding(.horizontal)
+                .padding(.trailing, 100)
+                .padding(.leading, 16)
+                Spacer()
+            }
+        }
     }
 }
