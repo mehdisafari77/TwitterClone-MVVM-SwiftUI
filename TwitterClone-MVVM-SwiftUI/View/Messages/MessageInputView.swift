3cf28ee10abba7a967012e06ce6fdc125f196943
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:22:07 -0700

    edts in messageview

--- TwitterClone-MVVM-SwiftUI/View/Messages/MessageInputView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/MessageInputView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct MessageInputView: View {
+    @Binding var messageText: String
+    
+    var action: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct MessageInputView_Previews: PreviewProvider {
-    static var previews: some View {
-        MessageInputView()
+        VStack {
+            Rectangle()
+                .foregroundColor(Color(.separator))
+                .frame(width: UIScreen.main.bounds.width, height: 0.75)
+                .padding(.bottom, 8)
+            
+            HStack {
+                TextField("Message...", text: $messageText)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .frame(minHeight: 30)
+                
+                Button(action: action) {
+                    Text("Send")
+                        .bold()
+                        .foregroundColor(.blue)
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
