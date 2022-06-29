1a39dc3e78b2f3c9f96ca4a7c267927e2be5dd8d
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:21:50 -0700

    edts in messageview

--- TwitterClone-MVVM-SwiftUI/View/Messages/ConversationCell.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ConversationCell.swift
@@ -6,15 +6,36 @@
 //
 
 import SwiftUI
+import KingfisherSwiftUI
 
 struct ConversationCell: View {
+    let message: Message
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ConversationCell_Previews: PreviewProvider {
-    static var previews: some View {
-        ConversationCell()
+        VStack(alignment: .leading) {
+            HStack(spacing: 12) {
+                KFImage(URL(string: message.user.profileImageUrl))
+                    .resizable()
+                    .scaledToFill()
+                    .clipped()
+                    .frame(width: 56, height: 56)
+                    .cornerRadius(28)
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(message.user.fullname)
+                        .font(.system(size: 14, weight: .semibold))
+                    
+                    Text(message.text)
+                        .font(.system(size: 15))
+                        .lineLimit(2)
+                }
+                .foregroundColor(.black)
+                .padding(.trailing)
+                
+                Spacer()
+            }
+            
+            Divider()
+        }
     }
 }
--- TwitterClone-MVVM-SwiftUI/View/Messages/ConversationsView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ConversationsView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct ConversationsView: View {
+    @State var isShowingNewMessageView = false
+    @State var showChat = false
+    @State var user: User?
+    @ObservedObject var viewModel = ConversationsViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .bottomTrailing) {
+            
+            if let user = user {
+                NavigationLink(destination: LazyView(ChatView(user: user)),
+                               isActive: $showChat,
+                               label: {} )
+            }
+            
+            ScrollView {
+                VStack {
+                    ForEach(viewModel.recentMessages) { message in
+                        NavigationLink(
+                            destination: LazyView(ChatView(user: message.user)),
+                            label: {
+                                ConversationCell(message: message)
+                            })
+                    }
+                }.padding()
+            }
+            
+            HStack {
+                Spacer()
+                
+                Button(action: { self.isShowingNewMessageView.toggle() }, label: {
+                    Image(systemName: "envelope")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 28, height: 28)
+                        .padding()
+                })
+                .background(Color(.systemBlue))
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .padding()
+                .sheet(isPresented: $isShowingNewMessageView, content: {
+                    NewMessageView(show: $isShowingNewMessageView, startChat: $showChat, user: $user)
+                })
+            }
+            .navigationTitle("Messages")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
