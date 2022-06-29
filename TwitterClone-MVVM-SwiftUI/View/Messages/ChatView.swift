45e50a81c0984cb8311cba85b70dcc34cd329159
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:19:58 -0700

    fixing file structure

---
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ChatView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatView()
+    }
+}
---
+++ TwitterClone-MVVM-SwiftUI/View/Messages/MessageInputView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageInputView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 29/06/2022.
+//
+
+import SwiftUI
+
+struct MessageInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageInputView()
+    }
+}
