9b8d142c8d6e5b7b21f6c16a17a87c48d6b14837
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:23:41 -0700

    new message view finish

--- TwitterClone-MVVM-SwiftUI/View/Messages/NewMessageView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/NewMessageView.swift
@@ -5,3 +5,33 @@
 //  Created by Mehdi MMS on 29/06/2022.
 //
 
+import SwiftUI
+
+struct NewMessageView: View {
+    @State var searchText = ""
+    @Binding var show: Bool
+    @Binding var startChat: Bool
+    @Binding var user: User?
+    @ObservedObject var viewModel = SearchViewModel(config: .newMessage)
+    
+    var body: some View {
+        ScrollView {
+            SearchBar(text: $searchText)
+                .padding()
+
+            VStack(alignment: .leading) {
+                ForEach(searchText.isEmpty ? viewModel.users : viewModel.filteredUsers(searchText)) { user in
+                    HStack { Spacer() }
+                    
+                    Button(action: {
+                        self.show.toggle()
+                        self.startChat.toggle()
+                        self.user = user
+                    }, label: {
+                        UserCell(user: user)
+                    })
+                }
+            }.padding(.leading)
+        }
+    }
+}
