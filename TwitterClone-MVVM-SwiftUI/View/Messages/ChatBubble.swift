f14b77b64de3dd4eed881229e0a09e779075b223
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:22:52 -0700

    dd chat bubble

--- TwitterClone-MVVM-SwiftUI/View/Messages/ChatBubble.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ChatBubble.swift
@@ -7,14 +7,18 @@
 
 import SwiftUI
 
-struct ChatBubble: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ChatBubble: Shape {
+    var isFromCurrentUser: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight, isFromCurrentUser ? .bottomLeft: .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
+        
+        return Path(path.cgPath)
     }
 }
 
 struct ChatBubble_Previews: PreviewProvider {
     static var previews: some View {
-        ChatBubble()
+        ChatBubble(isFromCurrentUser: false)
     }
 }
