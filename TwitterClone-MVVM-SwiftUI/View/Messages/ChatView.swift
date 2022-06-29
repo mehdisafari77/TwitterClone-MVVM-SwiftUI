aaabe0d3eaf2434beb6e6f0e79e1255a76e28c70
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:22:28 -0700

    finish chat view

--- TwitterClone-MVVM-SwiftUI/View/Messages/ChatView.swift
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ChatView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ChatView: View {
+    let user: User
+    @ObservedObject var viewModel: ChatViewModel
+    @State var messageText: String = ""
+    
+    init(user: User) {
+        self.user = user
+        self.viewModel = ChatViewModel(user: user)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(viewModel.messages) { message in
+                        MessageView(message: message)
+                    }
+                }
+            }.padding(.top)
+            
+            MessageInputView(messageText: $messageText, action: sendMessage)
+                .padding()
+            
+        }.navigationTitle(user.username)
+
+    }
+    
+    func sendMessage() {
+        viewModel.sendMessage(messageText)
+        messageText = ""
     }
 }
 
-struct ChatView_Previews: PreviewProvider {
-    static var previews: some View {
-        ChatView()
-    }
-}
+
