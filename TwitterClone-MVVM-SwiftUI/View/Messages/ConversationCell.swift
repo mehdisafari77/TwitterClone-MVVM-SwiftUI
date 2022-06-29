9406b0b4684c06f9b981bb1dea40af634b44ccdd
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-06-29 13:19:13 -0700

    fixing file structure

---
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ConversationCell.swift
@@ -0,0 +1,20 @@
+//
+//  ConversationCell.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ConversationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConversationCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationCell()
+    }
+}
---
+++ TwitterClone-MVVM-SwiftUI/View/Messages/ConversationsView.swift
@@ -0,0 +1,20 @@
+//
+//  ConversationsView.swift
+//  TwitterClone-MVVM-SwiftUI
+//
+//  Created by Mehdi MMS on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ConversationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConversationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationsView()
+    }
+}
